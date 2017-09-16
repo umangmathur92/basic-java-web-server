04c33470c86b7d5175b5274068bac8b8394a22c5
Umang Mathur <umangmathur10051992@gmail.com>
2017-09-15 17:57:06 -0700

    .

--- src/FileReaderTest.java
+++ src/FileReaderTest.java
@@ -4,7 +4,9 @@ public class FileReaderTest {
     public static void main(String[] args) {
         try {
             HttpdConf httpdConf = new HttpdConf("config/httpd.conf");
+
             Util.print(httpdConf.toString());
+
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/MimeTypes.java
+++ src/MimeTypes.java
@@ -3,11 +3,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.List;
 
 public class MimeTypes {
 
-    private HashMap<String, List<String>> mimeTypeMap = new HashMap<>();
+    private HashMap<String, String> mimeTypeMap = new HashMap<>();
 
     public MimeTypes(String filePathStr) throws IOException {
         Path filePath = Paths.get(filePathStr);
@@ -15,15 +14,15 @@ public class MimeTypes {
         parseInputStrAndSetAttributes(mimeTypeConfStr);
     }
 
-    public MimeTypes(HashMap<String, List<String>> mimeTypeMap) {
+    public MimeTypes(HashMap<String, String> mimeTypeMap) {
         this.mimeTypeMap = mimeTypeMap;
     }
 
-    public HashMap<String, List<String>> getMimeTypeMap() {
+    public HashMap<String, String> getMimeTypeMap() {
         return mimeTypeMap;
     }
 
-    public void setMimeTypeMap(HashMap<String, List<String>> mimeTypeMap) {
+    public void setMimeTypeMap(HashMap<String, String> mimeTypeMap) {
         this.mimeTypeMap = mimeTypeMap;
     }
 
